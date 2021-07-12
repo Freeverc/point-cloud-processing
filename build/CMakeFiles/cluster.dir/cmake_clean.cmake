130be1cf39b097dc4f48bfcd83aecbacec1568aa
freeverc <freeverchen@126.com>
2021-07-13 01:43:32 +0800

    init

---
+++ build/CMakeFiles/cluster.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cluster.dir/main.cpp.o"
+  "cluster.pdb"
+  "cluster"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cluster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
