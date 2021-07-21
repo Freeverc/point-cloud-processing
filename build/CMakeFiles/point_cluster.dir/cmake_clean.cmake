f42b0c9dda025a659b07d358a4afd0cd119a90a4
陈旭阳 <chenxuyang03@meituan.com>
2021-07-21 16:59:01 +0800

    init commit

---
+++ build/CMakeFiles/point_cluster.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/point_cluster.dir/main.cpp.o"
+  "point_cluster.pdb"
+  "point_cluster"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/point_cluster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
